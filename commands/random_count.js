3a34abb44053b284769c5a205e339130723a78ba
CwhiteKJ <dw696969@naver.com>
2021-03-26 22:31:24 +0900

    style: change code style

--- commands/random_count.js
+++ commands/random_count.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name: '숫자뽑기',
-  description: '0부터 자신이 말한수 중, 하나를 뽑습니다.',
-  aliases: ['숫자뽑기', 'random_count'],
-  category: 'bot',
+  name: "숫자뽑기",
+  description: "0부터 자신이 말한수 중, 하나를 뽑습니다.",
+  aliases: ["숫자뽑기", "random_count"],
+  category: "bot",
   execute(client, message, args, embed) {
     let count = Math.floor(Math.random() * args[0] + 1)
-    if (!count) return message.reply('숫자를 입력해주세요!')
-
-    message.reply(`${args}에서 ${count}를 뽑았습니다!`)
-  }
+    !count
+      ? message.reply("숫자를 입력해주세요!")
+      : message.reply(`${args}에서 ${count}를 뽑았습니다!`)
+  },
 }
