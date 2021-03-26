8f6c334521b85173c038e428c81f1847f8b0fb88
CwhiteKJ <dw696969@naver.com>
2021-03-26 22:32:38 +0900

    ESLint: remove unused variable

--- commands/random_count.js
+++ commands/random_count.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "0부터 자신이 말한수 중, 하나를 뽑습니다.",
   aliases: ["숫자뽑기", "random_count"],
   category: "bot",
-  execute(client, message, args, embed) {
+  execute(client, message, args) {
     let count = Math.floor(Math.random() * args[0] + 1)
     !count
       ? message.reply("숫자를 입력해주세요!")
