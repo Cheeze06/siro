3ed2fb20a223b88a328db3c9aa1ee781cfe5846c
Milly0120 <dmilly0120@gmail.com>
2021-03-28 17:43:40 +0900

    docs: category change

--- commands/ban.js
+++ commands/ban.js
@@ -2,13 +2,14 @@ module.exports = {
 	name: "벤",
 	description: "멘션한 유저를 서버에서 벤을 합니다!",
 	aliases: ["벤", "ban"],
-	category: "bot",
+	category: "moderation",
 	execute(client, message, args) {
 		const user = message.mentions.members.first()
 
 		// 예외처리
-		if (!user) return message.react("❌")
-		if (!message.member.hasPermission("BAN_MEMBERS")) return message.react("❌")
+		if (!user) return message.react("서버에서 벤하실 유저를 멘션해주세요")
+		if (!message.member.hasPermission("BAN_MEMBERS"))
+			return message.react("권한이 부족합니다")
 
 		// 벤
 		user
