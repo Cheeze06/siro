5fab7d348799423cadf9e6e4725de871fd0cdfc3
Milly0120 <dmilly0120@gmail.com>
2021-03-30 09:14:26 +0900

    feat: avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: "avatar",
+	description: "멘션한 유저의 프로필을 보여줍니다",
+	aliases: ["avatar", "프로필", "profile"],
+	category: "bot",
+	execute(client, message, args, embed) {
+		const user = message.mentions.users.first() || message.author
+
+		if (!user) {
+			embed.setImage(user.displayAvatarURL({ size: 2048, dynamic: true }))
+			message.reply(embed)
+		} else {
+			embed.setImage(user.displayAvatarURL({ size: 2048, dynamic: true }))
+			message.reply(embed)
+		}
+	},
+}
